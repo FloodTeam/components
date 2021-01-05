187cd17f48e5ff9ddacbb611a16131a65828f33d
Bobby Johnson <bobby@madnesslabs.net>
2021-01-04 22:18:27 -0600

    fix(pagination): Update with new logic from app repo

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,3 +1,4 @@
+import { ListQueryInput } from "@madnesslabs/thefloodteam-backend/dist/sdk";
 import {
   Component,
   ComponentInterface,
@@ -8,6 +9,8 @@ import {
   Listen,
   Method,
   Watch,
+  FunctionalComponent,
+  State,
 } from "@stencil/core";
 import Debounce from "debounce-decorator";
 
@@ -21,6 +24,8 @@ export class Pagination implements ComponentInterface {
 
   @Event() floodteamFetch: EventEmitter;
 
+  @Prop() gridEl: FunctionalComponent<any>;
+  @Prop() listEl: FunctionalComponent<any>;
   @Prop() disableFetch = false;
   @Prop({ mutable: true }) approxItemHeight: number;
   @Prop() endpoint?: string;
@@ -29,15 +34,25 @@ export class Pagination implements ComponentInterface {
   @Prop() limit?: number;
   @Prop() orderBy?: string;
   @Prop() orderDirection?: string;
-  @Prop() component = "div";
-  @Prop() componentProps: any;
   @Prop() dataPropsMap: any;
-  @Prop({ mutable: true }) page?: number;
+  @Prop() display: "list" | "grid" = "grid";
+  @Prop({ mutable: true }) page? = 0;
   @Prop({ mutable: true }) results: any[] = [];
+
+  @State() paramData: ListQueryInput = {};
 
   @Debounce(1000)
   @Watch("query")
   onQuery() {
+    this.results = [];
+    this.getResults({
+      page: 0,
+    });
+  }
+
+  @Watch("orderBy")
+  onOrderBy() {
+    this.results = [];
     this.getResults({
       page: 0,
     });
@@ -52,14 +67,15 @@ export class Pagination implements ComponentInterface {
         }
         this.page = event.detail?.data?.results?.page
           ? event.detail.data.results.page
-          : 0;
-        await this.addResults(event.detail.data.results.results);
+          : this.page + 1;
+        await this.addResults(event.detail.data.results);
       } catch (err) {
         console.log("Error updating results!");
       }
-      this.infiniteScrollEl.complete();
-      this.virtualScrollEl.checkEnd();
-      if (event.detail?.data?.results?.pageCount === this.page) {
+
+      await this.infiniteScrollEl.complete();
+      await this.virtualScrollEl.checkEnd();
+      if (!event.detail?.data?.results?.length) {
         this.infiniteScrollEl.disabled = true;
       }
     }
@@ -74,53 +90,99 @@ export class Pagination implements ComponentInterface {
 
   @Listen("resize", { target: "window" })
   onResize() {
-    let i;
-    let lastCol;
-    const cols = this.virtualScrollEl.querySelectorAll("ion-col");
-    for (i = 0; i < cols.length; i++) {
-      const col = cols[i];
-      if (lastCol && col.offsetTop !== lastCol.offsetTop) {
-        break;
+    if (
+      this.display === "list" &&
+      this.virtualScrollEl.querySelector("ion-item")
+    ) {
+      this.approxItemHeight = this.virtualScrollEl.querySelector(
+        "ion-item"
+      ).offsetHeight;
+    } else if (
+      this.display === "grid" &&
+      this.virtualScrollEl.querySelectorAll("ion-col")
+    ) {
+      let i;
+      let lastCol;
+      const cols = this.virtualScrollEl.querySelectorAll("ion-col");
+      for (i = 0; i < cols.length; i++) {
+        const col = cols[i];
+        if (lastCol && col.offsetTop !== lastCol.offsetTop) {
+          break;
+        }
+        lastCol = col;
       }
-      lastCol = col;
-    }
-    if (lastCol && lastCol.firstChild) {
-      this.approxItemHeight = lastCol.firstChild.scrollHeight / i + 18;
+      if (lastCol && lastCol.firstChild) {
+        this.approxItemHeight = lastCol.firstChild.scrollHeight / i + 18;
+      }
     }
   }
 
   @Method()
-  async addResults(results: any[] = []) {
-    this.onResize();
-    this.results = [...this.results, ...results];
+  async clearParamData(key?: string) {
+    if (key && this.paramData[key]) {
+      const paramData = this.paramData;
+      delete paramData[key];
+      this.paramData = paramData;
+    } else if (!key) {
+      this.paramData = {};
+    }
+
+    return this.paramData;
   }
 
-  getResults(
+  @Method()
+  async addResults(results: any[] = []) {
+    this.results = [...this.results, ...results];
+    setTimeout(() => {
+      this.onResize();
+    }, 1000);
+  }
+
+  @Method()
+  async clearResults() {
+    this.page = 0;
+    this.results = [];
+    setTimeout(() => {
+      this.onResize();
+    }, 1000);
+  }
+
+  @Method()
+  async getResults(
     options: {
       page?: number;
       next?: boolean;
-      limit?: boolean;
+      limit?: number;
+      paramData?: any;
     } = {}
   ) {
+    this.paramData = {
+      ...this.paramData,
+      limit: options.limit ? options.limit : this.limit,
+      orderBy: this.orderBy,
+      orderDirection: this.orderDirection,
+      ...(options?.paramData ? options.paramData : {}),
+    };
+
+    if (options.page) {
+      this.page = options.page;
+    }
+
+    if (this.query?.length > 1) {
+      this.paramData.query = this.query;
+    }
+
+    if (this.results?.length && this.results[this.results.length - 1]?.id) {
+      this.paramData.next = this.results[this.results.length - 1].id;
+    }
+
     this.floodteamFetch.emit({
       name: "pagination",
       endpoint: this.endpoint,
       dataPropsMap: this.dataPropsMap,
       disableFetch: this.disableFetch,
       params: {
-        data: this.fetchData
-          ? this.fetchData
-          : {
-              query: this.query ? this.query : "",
-              page: options.page
-                ? options.page
-                : options.next
-                ? this.page + 1
-                : this.page,
-              limit: options.limit ? options.limit : this.limit,
-              orderBy: this.orderBy,
-              orderDirection: this.orderDirection,
-            },
+        data: this.fetchData ? this.fetchData : this.paramData,
       },
     });
   }
@@ -130,7 +192,6 @@ export class Pagination implements ComponentInterface {
   }
 
   render() {
-    const ComponentTag = this.component;
     return (
       <div class="pagination">
         <ion-virtual-scroll
@@ -138,32 +199,39 @@ export class Pagination implements ComponentInterface {
           items={this.results}
           approxItemHeight={this.approxItemHeight}
         >
-          <ion-grid>
-            <ion-row>
-              {this.results.map((result) => {
-                return (
+          {this.display === "grid" ? (
+            <ion-grid>
+              <ion-row>
+                {this.results.map((result) => (
                   <ion-col>
-                    <ComponentTag
-                      {...(this.componentProps &&
-                      typeof this.componentProps === "function"
-                        ? this.componentProps(result)
-                        : {})}
-                    />
+                    {this.display === "grid"
+                      ? this.gridEl({ result }, null, null)
+                      : this.listEl({ result }, null, null)}
                   </ion-col>
-                );
-              })}
-            </ion-row>
-          </ion-grid>
-          <ion-infinite-scroll
-            style={{ display: "block" }}
-            ref={(el) => (this.infiniteScrollEl = el)}
-          >
-            <ion-infinite-scroll-content
-              loading-spinner="bubbles"
-              loading-text="Loading more data..."
-            ></ion-infinite-scroll-content>
-          </ion-infinite-scroll>
+                ))}
+              </ion-row>
+            </ion-grid>
+          ) : (
+            <ion-card>
+              <ion-list>
+                {this.results.map((result) =>
+                  this.display === "grid"
+                    ? this.gridEl({ result }, null, null)
+                    : this.listEl({ result }, null, null)
+                )}
+              </ion-list>
+            </ion-card>
+          )}
         </ion-virtual-scroll>
+        <ion-infinite-scroll
+          style={{ display: "block" }}
+          ref={(el) => (this.infiniteScrollEl = el)}
+        >
+          <ion-infinite-scroll-content
+            loading-spinner="bubbles"
+            loading-text="Loading more data..."
+          ></ion-infinite-scroll-content>
+        </ion-infinite-scroll>
       </div>
     );
   }
