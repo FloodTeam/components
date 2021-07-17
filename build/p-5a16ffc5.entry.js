ae227c90d6a04f6fa0e0b88fee35f8db0e9f8420
popcorn245 <popcorn245@users.noreply.github.com>
2021-07-17 21:16:19 +0000

    Deploying to gh-pages from master @ b0473fdcaff804265e38f229e69fe06474d91d06 🚀

--- build/p-5a16ffc5.entry.js
+++
@@ -1,1 +0,0 @@
-import{r as o,h as s,H as r}from"./p-a7779090.js";import{g as t}from"./p-eb85c436.js";import{c}from"./p-7840618d.js";const a=class{constructor(s){o(this,s)}render(){const o=t(this);return s(r,{class:c(this.color,{[o]:!0})},s("slot",null))}};a.style=":host(.ion-color){color:var(--ion-color-base)}";export{a as ion_text}
