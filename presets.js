6a07b00bde0e87b8177fc07927032c7dd01c64b2
popcorn245 <popcorn245@users.noreply.github.com>
2021-08-27 04:26:55 +0000

    Deploying to gh-pages from master @ ce648ed10b6405573b65adac03ca6b0e39b7d86f 🚀

--- presets.js
+++ presets.js
@@ -64,3 +64,24 @@ define("src/components/location-bar/location-bar.presets", ["require", "exports"
         }
     };
 });
+define("src/components/photo-carousel/photo-carousel.presets", ["require", "exports"], function (require, exports) {
+    "use strict";
+    exports.__esModule = true;
+    exports["default"] = {
+        "default": {
+            name: "Default",
+            props: {
+                photos: [
+                    {
+                        id: "test",
+                        url: "https://firebasestorage.googleapis.com/v0/b/ftms-ca85b.appspot.com/o/jobs%2FKJUk1VXGlRrALDFpNdOE%2Fphotos%2F2021-05-28T19%3A18%3A22.835Z.jpg?alt=media&token=f2617197-3249-4a65-852e-d3550ceef48b"
+                    },
+                    {
+                        id: "test2",
+                        url: "https://firebasestorage.googleapis.com/v0/b/ftms-ca85b.appspot.com/o/jobs%2FUXmgvBkzLJQ9eKRR8o7i%2Fphotos%2F2021-06-24T21%3A35%3A31.403Z.jpeg?alt=media&token=d5168723-3670-4b6e-872e-74a98d51180d"
+                    }
+                ]
+            }
+        }
+    };
+});
