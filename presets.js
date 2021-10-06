68454049489cf3de287ce145292f13a6fa2d52a1
popcorn245 <popcorn245@users.noreply.github.com>
2021-10-06 03:07:27 +0000

    Deploying to gh-pages from master @ 851cb696ac13f40283c3f5a98bb2870158de74a6 🚀

--- presets.js
+++ presets.js
@@ -30,7 +30,9 @@ define("src/components/epay/epay.presets", ["require", "exports"], function (req
             props: {
                 stripeKey: "pk_test_K1eh90TvFf2KeXn6HHTdUJRO",
                 dadeUrl: "https://test.dadesystems.com/api/billers/82",
-                dadeKey: "pk_test_K1eh90TvFf2KeXn6HHTdUJRO"
+                dadeKey: "pk_test_K1eh90TvFf2KeXn6HHTdUJRO",
+                amount: 100,
+                email: "popcorn245@gmail.com"
             }
         }
     };
