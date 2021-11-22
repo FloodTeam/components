a9130ba0a29470ba9799d4722d672fe920f99662
Bobby Johnson <bobby@madnesslabs.net>
2021-11-21 20:34:51 -0600

    fix(): Swap out fireenjin-designer for fireenjin docs

--- src/components/photo-carousel/photo-carousel.presets.ts
+++ src/components/photo-carousel/photo-carousel.presets.ts
@@ -1,4 +1,4 @@
-import { OrganismPresets } from "@madnesslabs/fireenjin-designer/dist/types/interfaces";
+import { OrganismPresets } from "@fireenjin/docs/dist/types/interfaces";
 
 export default {
   default: {
@@ -7,13 +7,13 @@ export default {
       photos: [
         {
           id: "test",
-          url: "https://firebasestorage.googleapis.com/v0/b/ftms-ca85b.appspot.com/o/jobs%2FKJUk1VXGlRrALDFpNdOE%2Fphotos%2F2021-05-28T19%3A18%3A22.835Z.jpg?alt=media&token=f2617197-3249-4a65-852e-d3550ceef48b"
+          url: "https://firebasestorage.googleapis.com/v0/b/ftms-ca85b.appspot.com/o/jobs%2FKJUk1VXGlRrALDFpNdOE%2Fphotos%2F2021-05-28T19%3A18%3A22.835Z.jpg?alt=media&token=f2617197-3249-4a65-852e-d3550ceef48b",
         },
         {
           id: "test2",
-          url: "https://firebasestorage.googleapis.com/v0/b/ftms-ca85b.appspot.com/o/jobs%2FUXmgvBkzLJQ9eKRR8o7i%2Fphotos%2F2021-06-24T21%3A35%3A31.403Z.jpeg?alt=media&token=d5168723-3670-4b6e-872e-74a98d51180d"
-        }
-      ]
-    }
+          url: "https://firebasestorage.googleapis.com/v0/b/ftms-ca85b.appspot.com/o/jobs%2FUXmgvBkzLJQ9eKRR8o7i%2Fphotos%2F2021-06-24T21%3A35%3A31.403Z.jpeg?alt=media&token=d5168723-3670-4b6e-872e-74a98d51180d",
+        },
+      ],
+    },
   },
 } as OrganismPresets;
